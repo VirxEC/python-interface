a4626c7ac6aec8cefc56844a511e8fa0d6d9050a
VirxEC <virx@virxcase.dev>
2024-07-28 15:58:08 -0400

    Remove date from logging

--- rlbot/utils/logging.py
+++ rlbot/utils/logging.py
@@ -38,7 +38,7 @@ class CustomFormatter(logging.Formatter):
         for color, section in zip(colors, self.SECTIONS):
             log_fmt += color + section
 
-        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        formatter = logging.Formatter(log_fmt, datefmt="%H:%M:%S")
         return formatter.format(record)
 
 
